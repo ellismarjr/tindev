5325c930db282f1ebf1a6cd20867673919c74a23
Júnior de Oliveira <ellismarjr@hotmail.com>
2019-08-09 11:44:08 -0300

    Upgrade Backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,40 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const routes = require('./routes')
 
-const server = express()
+const app = express()
+
+// aceita conexões websocket quanto http
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+const connectedUsers = {
+
+}
+
+io.on('connection', socket => {
+  const { user } = socket.handshake.query
+
+  console.log(user, socket.id)
+  connectedUsers[user] = socket.id
+})
+
+
 // Conectando no servidor Mongo
 mongoose.connect('mongodb+srv://tindev:tindev@cluster0-sdw2v.mongodb.net/tindev?retryWrites=true&w=majority', {useNewUrlParser: true})
 
-server.use(cors())
-// diz ao Node que iremos utilizar forma json nas requisições
-server.use(express.json())
-server.use(routes)
+app.use((req, res, next) => {
+  req.io = io
+  req.connectedUsers = connectedUsers
 
-server.listen(3333)
+  return next()
+})
+
+app.use(cors())
+// diz ao Node que iremos utilizar forma json nas requisições
+app.use(express.json())
+app.use(routes)
+
+const portProduction = process.env.PORT || 8080;
+// const portDev = 3333
+
+server.listen(portProduction)
